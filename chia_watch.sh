5fe515d4dff73c23c2e770de04aed95a3341a56d
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-09-06 22:06:04 +0200

    Update chia_watch.sh
    Introduced variables
    .) loading_plots to prevent chia_watch to believe start procedure is finished to early
    .) osuser and blockchain_name, default use for chia
    .) node_role to select farmer or harvester for restart command
    .) initial_local_connects to establish certain connections after automatic restart

--- chia_watch.sh
+++ chia_watch.sh
@@ -2,104 +2,134 @@
 #initialize variables until first eligible
 seconds_without_eligible_until_restart="90"
 #seconds_until_write_report="900"
-seconds_until_write_report="300"
+seconds_until_write_report="900"
 last_eligible=`date +%s`
 seconds_since_last_eligible="0"
 last_report=`date +%s`
 seconds_since_last_report="0"
 eligible_interval_count="0"
-farmer_restart_active="no"
+#Parameter used to distinguish between restart or normal operation
+restart_active="no"
+#Parameter used to prevent buffered lines containing eligible string to be used
+loading_plots="no"
+
+#osuser
+osuser=chia
+#chia/flax/..
+blockchain_name=chia
+
+#Node Role
+#farmer/harvester
+node_role=farmer
 
 #path/files
-chia_logdir=~chia/.chia/mainnet/log
-chia_rundir=~chia/.chia/mainnet/run
-chia_blockchain=~chia/chia-blockchain
+#logdir=~chia/.chia/mainnet/log
+#rundir=~chia/.chia/mainnet/run
+blockchain=`eval echo "~$osuser"/${blockchain_name}-blockchain`
+logdir=`eval echo "~$osuser"/.${blockchain_name}/mainnet/log`
+rundir=`eval echo "~$osuser"/.${blockchain_name}/mainnet/run`
 restartlog=restart_triggered_by_eligible_time.out
 reportlog=watchlog_report.out
 
 #cleanup
-[ -f $chia_logdir/trigger.restart ] && rm $chia_logdir/trigger.restart
+[ -f $logdir/trigger.restart ] && rm $logdir/trigger.restart
 
-#assign current farmer pid
-current_farmer_pid=`cat $chia_rundir/chia_farmer.pid`
+#assign current $node_role pid
+current_pid=`cat $rundir/${blockchain_name}_${node_role}.pid`
+
+#add local connections after restart
+initiate_local_connects="yes"
 
 #LOOP
-tail -Fn0 $chia_logdir/debug.log | \
+tail -Fn0 $logdir/debug.log | \
 while read line ; do
                 #Check for eligible plots
-                if [ $farmer_restart_active = "no" ]
+                if [ $restart_active = "no" ]
                 then
                         #check for eligible plots and assign current timestamp
                         echo "$line" | grep -q "plots were eligible" && last_eligible=`date +%s` && ((eligible_interval_count+=1))
                         #calculate seconds since last eligible timestamp
                         seconds_since_last_eligible=$(($(date +%s)-$last_eligible))
                 #Check for eligible plots during restart
-                elif [ $farmer_restart_active = "yes" ]
+                elif [ $restart_active = "yes" ]
                 then
-                        echo "$line" | grep -q "plots were eligible" && last_eligible=`date +%s` && farmer_restart_active="no" && ((eligible_interval_count+=1))
-                        if [ $farmer_restart_active = "no" ]
+                        if [ $loading_plots = "no" ]
                         then
-                                echo "Restart finished: $(date +%Y-%m-%d_%H-%M-%S)"                                             | tee -a $chia_logdir/$restartlog
-                                #add connection to local full node
-                                #chia show -a 192.168.1.164:8444                                                                 | tee -a $chia_logdir/$restartlog
-                                #assign new farmer pid
-                                current_farmer_pid=`cat $chia_rundir/chia_farmer.pid`
-                                echo "#++++++++++++++++++++++++++++++++++++#"                                                   | tee -a $chia_logdir/$restartlog
+                                echo $line | grep -q "Found plot" && loading_plots="yes"
+                        elif [ $loading_plots = "yes" ]
+                        then
+                                echo "$line" | grep -q "plots were eligible" && last_eligible=`date +%s` && restart_active="no" && loading_plots="no" && ((eligible_interval_count+=1))
+                        fi
+                        #TEST
+                        #echo $line | tee -a $logdir/$restartlog
+                        if [ $restart_active = "no" ]
+                        then
+                                echo "Restart finished: $(date +%Y-%m-%d_%H-%M-%S)"                                             | tee -a $logdir/$restartlog
+                                if [ $initiate_local_connects = "yes" ]
+                                then
+                                        #wait some seconds so full node can be fully started before initiating local connection
+                                        #can in future be checked by string "registered for service chia_full_node"
+                                        sleep 30
+                                        #add connection to local full node
+                                        $blockchain_name show -a 192.168.1.164:8444                                             | tee -a $logdir/$restartlog
+                                fi
+                                #assign new $node_role pid
+                                current_pid=`cat $rundir/${blockchain_name}_${node_role}.pid`
+                                echo "#++++++++++++++++++++++++++++++++++++#"                                                   | tee -a $logdir/$restartlog
                         fi
                 fi
                 #TEST:trigger.restart for testing purpose
                 #Check if no eligible plots were found in expected timeframe
-                if [ "$seconds_since_last_eligible" -gt "$seconds_without_eligible_until_restart" ] && ps -p $current_farmer_pid > /dev/null || [ -f $chia_logdir/trigger.restart ]
+                if [ "$seconds_since_last_eligible" -gt "$seconds_without_eligible_until_restart" ] && ps -p $current_pid > /dev/null || [ -f $logdir/trigger.restart ]
                 then
-                        . $chia_blockchain/activate
-                        echo "#------------------------------------#"                                                           | tee -a $chia_logdir/$restartlog
-                        echo "#Triggering restart#"                                                                             | tee -a $chia_logdir/$restartlog
-                        echo "Reason: eligible threshold reached"                                                               | tee -a $chia_logdir/$restartlog
-                        echo "Seconds since last eligible: $seconds_since_last_eligible"                                        | tee -a $chia_logdir/$restartlog
-                        echo "Restart Threshold(seconds): $seconds_without_eligible_until_restart"                              | tee -a $chia_logdir/$restartlog
-                        echo "Unix Timestamp: $(date +%s)"                                                                      | tee -a $chia_logdir/$restartlog
-                        echo "Restart triggered: $(date +%Y-%m-%d_%H-%M-%S)"                                                    | tee -a $chia_logdir/$restartlog
-                        chia start farmer -r                                                                                    | tee -a $chia_logdir/$restartlog
-                        farmer_restart_active="yes"
+                        . $blockchain/activate
+                        echo "#------------------------------------#"                                                           | tee -a $logdir/$restartlog
+                        echo "#Triggering restart#"                                                                             | tee -a $logdir/$restartlog
+                        echo "Reason: eligible threshold reached"                                                               | tee -a $logdir/$restartlog
+                        echo "Seconds since last eligible: $seconds_since_last_eligible"                                        | tee -a $logdir/$restartlog
+                        echo "Restart Threshold(seconds): $seconds_without_eligible_until_restart"                              | tee -a $logdir/$restartlog
+                        echo "Unix Timestamp: $(date +%s)"                                                                      | tee -a $logdir/$restartlog
+                        echo "Restart triggered: $(date +%Y-%m-%d_%H-%M-%S)"                                                    | tee -a $logdir/$restartlog
+                        $blockchain_name start $node_role -r                                                                        | tee -a $logdir/$restartlog
+                        restart_active="yes"
                         seconds_since_last_eligible="0"
-                        current_farmer_pid=`cat $chia_rundir/chia_farmer.pid`
+                        current_pid=`cat $rundir/${blockchain_name}_${node_role}.pid`
                         #TEST
-                        rm $chia_logdir/trigger.restart
+                        rm $logdir/trigger.restart
                 #Farmer Restart not triggered by watch
-                elif ! ps -p $current_farmer_pid > /dev/null && [ $farmer_restart_active = "no" ]
+                elif ! ps -p $current_pid > /dev/null && [ $restart_active = "no" ]
                 then
-                        echo "#------------------------------------#"                                                           | tee -a $chia_logdir/$restartlog
-                        echo "#No farmer pid detected#"                                                                         | tee -a $chia_logdir/$restartlog
-                        echo "#Farmer was probably stopped manually#"                                                           | tee -a $chia_logdir/$restartlog
-                        echo "Unix Timestamp: $(date +%s)"                                                                      | tee -a $chia_logdir/$restartlog
-                        echo "Downtime detected: $(date +%Y-%m-%d_%H-%M-%S)"                                                    | tee -a $chia_logdir/$restartlog
-                        farmer_restart_active="yes"
+                        echo "#------------------------------------#"                                                           | tee -a $logdir/$restartlog
+                        echo "#No $node_role pid detected#"                                                                         | tee -a $logdir/$restartlog
+                        echo "#Farmer was probably stopped manually#"                                                           | tee -a $logdir/$restartlog
+                        echo "Unix Timestamp: $(date +%s)"                                                                      | tee -a $logdir/$restartlog
+                        echo "Downtime detected: $(date +%Y-%m-%d_%H-%M-%S)"                                                    | tee -a $logdir/$restartlog
+                        restart_active="yes"
                         seconds_since_last_eligible="0"
                         when_downtime_detected=$(date +%s)
-                        # Wait until chia farmer is started again
-                        while [ ! -f $chia_rundir/chia_farmer.pid ]; do
+                        # Wait until $blockchain_name $node_role is started again
+                        while [ ! -f $rundir/${blockchain_name}_${node_role}.pid ]; do
                                 sleep 30
                                 time_waited=$(($(date +%s)-$when_downtime_detected))
-                                echo "Waited for farmer restart: $time_waited seconds"                                          | tee -a $chia_logdir/$restartlog
+                                echo "Waited for $node_role restart: $time_waited seconds"                                          | tee -a $logdir/$restartlog
                         done
-                        echo "Startup was triggered, waiting for plots to be loaded."                                           | tee -a $chia_logdir/$restartlog
+                        echo "Startup was triggered, waiting for plots to be loaded."                                           | tee -a $logdir/$restartlog
                 fi
                 seconds_since_last_report=$(($(date +%s)-$last_report))
                 #TEST
                 #echo $seconds_since_last_eligible
                 #echo $seconds_since_last_report
-                if [ "$seconds_since_last_report" -gt "$seconds_until_write_report" ] && [ $farmer_restart_active = "no" ]
+                if [ "$seconds_since_last_report" -gt "$seconds_until_write_report" ] && [ $restart_active = "no" ]
                 then
-                        . $chia_blockchain/activate
-                        echo "#------------------------------------#"                                                           | tee -a $chia_logdir/$reportlog
-                        echo "#Report of last $seconds_since_last_report seconds #"                                             | tee -a $chia_logdir/$reportlog
-                        echo "Seconds since last eligible: $seconds_since_last_eligible"                                        | tee -a $chia_logdir/$reportlog
-                        echo "Unix Timestamp: $(date +%s)"                                                                      | tee -a $chia_logdir/$reportlog
-                        echo "Current Timestamp: $(date +%Y-%m-%d_%H-%M-%S)"                                                    | tee -a $chia_logdir/$reportlog
-                        echo "Checked for eligible plots: $eligible_interval_count times"                                       | tee -a $chia_logdir/$reportlog
+                        . $blockchain/activate
+                        echo "#------------------------------------#"                                                           | tee -a $logdir/$reportlog
+                        echo "#Report of last $seconds_since_last_report seconds #"                                             | tee -a $logdir/$reportlog
+                        echo "Seconds since last eligible: $seconds_since_last_eligible"                                        | tee -a $logdir/$reportlog
+                        echo "Unix Timestamp: $(date +%s)"                                                                      | tee -a $logdir/$reportlog
+                        echo "Current Timestamp: $(date +%Y-%m-%d_%H-%M-%S)"                                                    | tee -a $logdir/$reportlog
+                        echo "Checked for eligible plots: $eligible_interval_count times"                                       | tee -a $logdir/$reportlog
                         last_report=`date +%s`
                         eligible_interval_count="0"
                         seconds_since_last_report="0"
                 fi
-                #Write summary of signage points to logfile
 done
